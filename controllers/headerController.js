ba6b50edfda0f7d8c0d652535e24a085326c6c63
Pardeep-Altudo <pradeep.singh@altudo.co>
2024-09-22 13:59:19 +0530

    Header controller added

--- controllers/headerController.js
+++ controllers/headerController.js
@@ -3,7 +3,7 @@ const Header = require("../models/Header");
 // Get header data
 const getHeaderData = async (req, res) => {
   try {
-    const headerData = await Header.find();
+    const headerData = await Header.findOne({}, { _id: 0, __v: 0 });
     res.status(200).json(headerData);
   } catch (error) {
     res.status(500).json({ message: "Error fetching header data", error });
@@ -13,9 +13,12 @@ const getHeaderData = async (req, res) => {
 // Create header data
 const createHeaderData = async (req, res) => {
   try {
-    console.log(req.body);
+    const headerData = new Header(req.body);
+    await headerData.save();
+    res.status(201).json({ message: "Header data created" });
   } catch (error) {
-    res.status(500).json({ message: "Error fetching header data", error });
+    console.log(error, "ERRO");
+    res.status(500).json({ message: "Error creating header data", error });
   }
 };
 
